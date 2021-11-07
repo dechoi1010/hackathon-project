63a86277e89fb934fe2dbc3dafff153bf37e7b95
Subin <bina@sookmyung.ac.kr>
2021-11-07 16:56:07 +0900

    todoing

---
+++ js/greet.js
@@ -0,0 +1,35 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector(".login_box");
+const greet = document.querySelector(".greeting");
+const greetMessage = greet.querySelector(".greet_message");
+const bg = document.querySelector(".background_overlay");
+const todo = document.querySelector(".todo_box");
+
+const HIDDEN_CLASS = "hidden";
+const USER_KEY = "username";
+
+function formSubmit(event) {
+  event.preventDefault();
+  const username = loginInput.value;
+  loginForm.classList.add(HIDDEN_CLASS);
+  localStorage.setItem(USER_KEY, username);
+  paintgGreet(username);
+}
+
+function paintgGreet(username) {
+  greetMessage.innerText = `hello ${username}`;
+  greet.classList.remove(HIDDEN_CLASS);
+  bg.classList.remove(HIDDEN_CLASS);
+  todo.classList.remove(HIDDEN_CLASS);
+}
+
+const savedUser = localStorage.getItem(USER_KEY);
+
+if (savedUser === null) {
+  //show the form
+  loginForm.classList.remove(HIDDEN_CLASS);
+  loginForm.addEventListener("submit", formSubmit);
+} else {
+  //show the greet
+  paintgGreet(savedUser);
+}
