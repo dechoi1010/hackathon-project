7c6708179878d3f91540ddb1a67949e79577c349
Daeun Choi <dechoi1127@gmail.com>
2021-11-07 19:50:56 +0900

    4th edited

--- js/clock.js
+++ js/clock.js
@@ -1,14 +1,16 @@
-
 const clock = document.querySelector(".time");
 
 function paintClock() {
   const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+
   const hours = String(date.getHours()).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2, "0");
   const seconds = String(date.getSeconds()).padStart(2, "0");
-  clock.innerText = `${hours}:${minutes}:${seconds}`;
+  clock.innerText = `${year}년 ${month}월 ${day}일, ${hours}:${minutes}:${seconds}`;
 }
 
 paintClock();
 setInterval(paintClock, 1000);
-
