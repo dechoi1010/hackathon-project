03e077bb4a8ab8e7da92e7f3aab93476b77e2e9e
Subin <bina@sookmyung.ac.kr>
2021-11-07 20:21:06 +0900

    a

--- js/todo.js
+++ js/todo.js
@@ -1,3 +1,4 @@
+
 /*    
     <div class="todo_box">
       <form class="todo_form">
@@ -9,9 +10,13 @@
 
 const todoForm = document.querySelector(".todo_form");
 const todoList = document.querySelector(".todo_list");
+let whatTodo = true;
+
 let toDos = [];
+let weekToDos = [];
 let newTodo = "";
 const TODO_KEY = "todos";
+const WEEK_TODO_KEY = "weektodos";
 
 function toDoSubmit(event) {
     event.preventDefault();
@@ -23,8 +28,14 @@ function toDoSubmit(event) {
         text: newTodo,
         id: Date.now(),
     };
+    newTodoObj.text = newTodo;
+    newTodoObj.id = Date.now();
     paintToDo(newTodoObj);
-    toDos.push(newTodoObj);
+    if (whatTodo) {
+        toDos.push(newTodoObj);
+    } else {
+        weekToDos.push(newTodoObj);
+    }
     saveToDoFunc();
 }
 
@@ -54,21 +65,67 @@ function paintToDo(newTodoObj) {
 
 function deleteToDo(event) {
     const li = event.target.parentElement;
-    toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+    if (whatTodo) {
+        toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+    } else {
+        weekToDos = weekToDos.filter((toDo) => toDo.id !== parseInt(li.id));
+    }
     li.remove();
     saveToDoFunc();
 }
 
 function saveToDoFunc() {
-    localStorage.setItem(TODO_KEY, JSON.stringify(toDos));
+    if (whatTodo) {
+        localStorage.setItem(TODO_KEY, JSON.stringify(toDos));
+    } else {
+        localStorage.setItem(WEEK_TODO_KEY, JSON.stringify(weekToDos));
+    }
 }
 
-const savedToDos = localStorage.getItem(TODO_KEY);
 
-if(savedToDos) {
-    const parsedToDos = JSON.parse(savedToDos);
-    toDos = parsedToDos;
-    parsedToDos.forEach(paintToDo);
+
+function resetToDo() {
+    while (todoList.firstChild) {
+        todoList.firstChild.remove()
+    }
+}
+
+function a() {
+    const savedToDos = localStorage.getItem(TODO_KEY);
+    const savedWeekToDos = localStorage.getItem(WEEK_TODO_KEY);
+    todoForm.classList.remove("hidden");
+    if(savedToDos && whatTodo) {
+        // 위에 textspan 추가하든가 해야하네 아님 추가해놓고 text를 바꾸든가
+        //아 박스도 block 시켜야 하네... alclrpTek
+        
+        resetToDo();
+        const parsedToDos = JSON.parse(savedToDos);
+        toDos = parsedToDos;
+        console.log(toDos);
+        parsedToDos.forEach(paintToDo);
+    } else if(savedWeekToDos) {
+        resetToDo();
+        const parsedWeekToDos = JSON.parse(savedWeekToDos);
+        weekToDos = parsedWeekToDos;
+        parsedWeekToDos.forEach(paintToDo);
+    } else {
+        resetToDo();
+    }
 }
 
 todoForm.addEventListener("submit", toDoSubmit);
+
+
+const todayList = document.querySelector('.nav__link#today-list');
+const weekList = document.querySelector('.nav__link#week-list');
+console.log(todayList);
+console.log(weekList);
+
+
+todayList.addEventListener("click", () => {whatTodo = true; a()});
+weekList.addEventListener("click", () => {whatTodo = false; a()});
+
+
+
+
+
